957159ebbdbd525dcb47a8c7cdb5fac0117cefb7
cyberareauzb <cyberareauzb@gmail.com>
2022-06-27 20:21:52 +0500

    Initial commit

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/local/StockDao.kt
@@ -0,0 +1,22 @@
+package com.plcoding.stockmarketapp.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface StockDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCompanyListings(
+        companyListingEntity: List<CompanyListingEntity>
+    )
+
+    @Query("DELETE FROM companylistingentity")
+    suspend fun clearCompanyListing()
+
+
+
+
+}
---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -0,0 +1,21 @@
+package com.plcoding.stockmarketapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("api_key") apiKey: String
+    ):ResponseBody
+
+    companion object {
+        const val API_KEY = "RMATTHF4842JQ1LE"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+
+}
